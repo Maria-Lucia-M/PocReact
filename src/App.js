437a803a4cdc47b357071a8baa299ab569c20a7d
Maria Lucia Munne <marilumunne@gmail.com>
2024-10-23 07:56:37 -0300

    correccion3

--- src/App.js
+++ src/App.js
@@ -7,12 +7,10 @@ function App() {
     <div className="App">
        <CurrencyPesoConverter />
        <header className="App-header">
-        <a href='https://www.youtube.com/watch?v=BbeeuzU5Qc8'target="_blank" rel="noopener noreferrer">
         <img
-          src="https://www.nme.com/wp-content/uploads/2023/09/kevin-james-meme.jpg"
-          alt="Kevin James meme"
+          src="https://imgs.search.brave.com/R4rvRIOnfVnQrM5hBPqiWb6vg4Q_zcGXp9L6XbG1mMg/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9yZXNp/emVyLmdsYW5hY2lv/bi5jb20vcmVzaXpl/ci92Mi9kb2xhci10/YXJqZXRhLWhveS1h/LWN1YW50by1jb3Rp/emEtZWwtZG9sYXIt/VjdSTTI1SkNPTkU2/REZKSjI1NUVDRllZ/QkEuanBnP2F1dGg9/MTkxYTI2MDcwNTZj/OGYwZTJjYjIyZGE5/MzI4MjYxZjg2NWU3/NmE3ZmJmNWNlNzBl/YjI0N2JmZWQxZGMy/M2I1MiZ3aWR0aD0z/MDAmaGVpZ2h0PTIw/MCZxdWFsaXR5PTcw/JnNtYXJ0PXRydWU"
+          alt="Dolar"
           style={{ width: '300px', height: 'auto' }}/>
-        </a>
       </header>
     </div>
   );
